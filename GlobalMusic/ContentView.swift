d1ac52758b3aee7e28e1c0b20e58ef0e5e001157
LuisRamirez <ramirezluis0111@gmail.com>
2021-07-02 12:13:52 -0300

    Initial Commit

---
+++ GlobalMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlobalMusic
+//
+//  Created by Ramirez Luis on 02/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlobalMusic/GlobalMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalMusicApp.swift
+//  GlobalMusic
+//
+//  Created by Ramirez Luis on 02/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GlobalMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
