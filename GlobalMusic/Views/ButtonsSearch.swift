b319749a4c5e04d0ea2a3dea3434f7b570df4d68
LuisRamirez <ramirezluis0111@gmail.com>
2021-07-11 17:50:01 -0300

    Fix Search, Lists and play content

---
+++ GlobalMusic/Views/ButtonsSearch.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonsSearch.swift
+//  GlobalMusic
+//
+//  Created by Ramirez Luis on 06/07/2021.
+//
+
+import SwiftUI
+
+struct ButtonsSearch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonsSearch_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonsSearch()
+    }
+}
